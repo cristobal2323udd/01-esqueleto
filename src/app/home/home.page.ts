ece5280eb61e136fa5382be444d495aa7e5e45e2
Cristobal Maturana <cristobal.maturana@udd.cl>
2026-06-06 16:32:48 -0400

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+})
+export class HomePage {}
